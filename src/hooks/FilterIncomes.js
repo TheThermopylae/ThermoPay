c7439c4b036354ddba5b4a8fe1f7ea500755e3a8
TheThermopylae <amirmahdi.brzgr1386@gmail.com>
2025-01-07 22:33:18 +0330

    Sort Incomes (V 1.1.3)

--- src/hooks/FilterIncomes.js
+++ src/hooks/FilterIncomes.js
@@ -4,16 +4,17 @@ export default function FilterIncomes () {
   let userData = inject('userData')
   let incomes = inject('incomes')
 
-  let userIncomes = computed(() =>
-    incomes.value.filter(income => income[1].for == userData.value.name)
-  )
+  let userIncomes = computed(() => {
+    let findIncomes = incomes.value.filter(
+      income => income[1].for == userData.value.name
+    )
+    return findIncomes.slice().sort((a, b) => a[1].year - b[1].year)
+  })
 
   let lastYearIncome = computed(() => {
     let date = new Date().getFullYear()
 
-    let lastYear = userIncomes.value.find(
-      income => income[1].year == date
-    )
+    let lastYear = userIncomes.value.find(income => income[1].year == date)
     return lastYear
   })
   return [userIncomes, lastYearIncome]
